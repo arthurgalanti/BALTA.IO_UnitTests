b13ba1c7b7b7e38ebfeeb9f5ec3b5e69a9486bea
ArthurGalanti <arthur.galanti@hotmail.com>
2023-05-11 23:48:25 -0300

    Adicionado Queries e Repositories

---
+++ Store.Domain/Repositories/IDeliveryFeeRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Store.Domain.Repositories.Interfaces;
+
+public interface IDeliveryFeeRepository
+{
+    decimal Get(string zipCode);
+}
---
+++ Store.Domain/Repositories/IDiscountRepository.cs
@@ -0,0 +1,8 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Domain.Repositories.Interfaces;
+
+public interface IDiscountRepository
+{
+    Discount Get(string code);
+}
